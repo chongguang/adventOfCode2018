b18118fc5cebb70e27f8e96560c8cfcd8f51b536
Chongguang LIU <chongguang.liu@socgen.com>
2018-12-09 14:07:57 +0100

    q2 d8

--- day8.py
+++ day8.py
@@ -5,6 +5,7 @@ nbs = content[0].split(' ')
 nbs = [int(x) for x in nbs]
 print(nbs)
 
+"""
 
 children_stack = [9]
 metadata_stack = [11]
@@ -35,6 +36,46 @@ while pointer < len(nbs) and len(children_stack) > 0:
 
 
 print(metadata_sum)
+"""
+
+class Node:
+    def __init__(self, start):
+        self.p = start
+        self.nb_children = nbs[start]
+        self.nb_metadata = nbs[start + 1]
+        self.values = []
+        self.current_child = None
+
+    def calculate_value(self):
+        if self.nb_children == 0:
+            v = 0
+            for i in range(self.p + 2, self.p + 2 + self.nb_metadata):
+                v =  v + nbs[i]
+            end = self.p + 2 + self.nb_metadata
+            return v, end
+        else:
+            self.p = self.p + 2
+            while self.nb_children > 0:
+                # create current child
+                self.current_child = Node(self.p)
+                # add current child's value to self.values[]
+                value_and_p = self.current_child.calculate_value()
+                self.values.append(value_and_p[0])
+                # move self.p forward
+                self.p = value_and_p[1]
+                # self.nb_children --
+                self.nb_children = self.nb_children - 1
+
+            total_value = 0
+            for i in range(self.p, self.p + self.nb_metadata):
+                if nbs[i] <= len(self.values):
+                    total_value = total_value + self.values[nbs[i] - 1]
+            return total_value, self.p + self.nb_metadata
+
+
+n = Node(0)
+
+print(n.calculate_value())
 
 
 
